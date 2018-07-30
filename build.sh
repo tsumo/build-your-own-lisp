44c999c566d4f93fd9abd1754ad5be258a1cc6e3
tsumo <tsumo@users.noreply.github.com>
2018-07-30 18:09:20 +0300

    hello world, build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "File not found"
+    exit 2
+fi
+
+mkdir -p bin
+
+cc -std=c99 -Wall "$1" -o "./bin/${1%.*}"
+
