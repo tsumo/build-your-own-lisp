6378746339609b2f8491b931f80240a4c93150b8
tsumo <tsumo@users.noreply.github.com>
2019-10-20 12:08:55 +0300

    Eval function for math operations

--- parsing.c
+++ parsing.c
@@ -5,6 +5,39 @@
 #include <editline/readline.h>
 /* #include <editline/histedit.h> */
 
+
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+
+long eval(mpc_ast_t* t) {
+    /* If tagged as number - return it directly */
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always the second child, "(" is the first one */
+    char* op = t->children[1]->contents;
+
+    /* Store third child in x */
+    long x = eval(t->children[2]);
+
+    /* Iterate through remaining children and combine results */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
+
 int main(int argc, char** argv) {
 
     /* Parsers */
@@ -34,7 +67,9 @@ int main(int argc, char** argv) {
         /* Parse input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            mpc_ast_print(r.output);
+            /* Evaluate parse tree */
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
