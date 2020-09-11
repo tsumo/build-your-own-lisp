8fbd9b165400aff644bd3506ff6187f57e40044c
tsumo <tsumo@users.noreply.github.com>
2020-09-11 23:08:21 +0300

    Simple REPL setup

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,5 +1,33 @@
-type a = { name: string };
+const addInputListener = (
+  inputEl: HTMLTextAreaElement,
+  outputEl: HTMLDivElement
+) => {
+  const keyDownListener = (e: KeyboardEvent) => {
+    if (e.ctrlKey && e.code === "Enter") {
+      e.preventDefault();
+      if (inputEl.value.trim().length === 0) {
+        return;
+      }
+      outputEl.innerHTML += `${inputEl.value.trim()}<br />`;
+      outputEl.scrollTo({ top: outputEl.scrollHeight });
+      inputEl.value = "";
+    }
+  };
+  inputEl.addEventListener("keydown", keyDownListener);
+};
 
-const v: a = { name: "hi!" };
+const init = () => {
+  const inputEl = document.getElementById("input");
+  const outputEl = document.getElementById("output");
+  if (
+    !(inputEl instanceof HTMLTextAreaElement) ||
+    !(outputEl instanceof HTMLDivElement)
+  ) {
+    console.error("Couldn't get dom elements");
+    return;
+  }
+  inputEl.focus();
+  addInputListener(inputEl, outputEl);
+};
 
-console.log(v);
+window.onload = init;
